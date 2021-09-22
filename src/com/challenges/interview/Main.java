a55087122d393c40738daceea21177e3ee9675ab
Oscar Daniel Guzmán Lozano <ddkdaniel@gmail.com>
2021-09-22 11:28:45 -0500

    Fill with zeroes
    This part of the program creates a Matrix opf NxN and fills it with random numbers, then processes the matrix and fills with zeros the row and column where it finds a zero in the original matrix, then prints the modified version

--- src/com/challenges/interview/Main.java
+++ src/com/challenges/interview/Main.java
@@ -1,8 +1,68 @@
 package com.challenges.interview;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        final int N = 4;
+        int[][] array = new int[N][N];
+
+        fillArrayWithRandom(array);
+
+        printMatrix(array);
+
+        substituteZeroes(array);
+
+
+
+    }
+
+    private static void printMatrix(int[][] array) {
+        for (int[] i: array) {
+            System.out.println(Arrays.toString(i));
+        }
+    }
+
+    private static void substituteZeroes(int[][] array) {
+        ArrayList<Integer> rowsToZero = new ArrayList<>();
+        ArrayList<Integer> columnsToZero = new ArrayList<>();
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] == 0) {
+                    if (!rowsToZero.contains(i)) {
+                        rowsToZero.add(i);
+                    }
+                    if (!columnsToZero.contains(j)) {
+                        columnsToZero.add(j);
+                    }
+                }
+            }
+        }
+
+        for (Integer row : rowsToZero) {
+            Arrays.fill(array[row], 0);
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            for (Integer col : columnsToZero) {
+                array[i][col] = 0;
+            }
+        }
+
+        System.out.println();
+        printMatrix(array);
+    }
+
+    private static void fillArrayWithRandom(int[][] array) {
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                array[i][j] = random.nextInt(9);
+            }
+        }
     }
 }
